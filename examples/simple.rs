a06af92660b36d78e9d440184309227036983f14
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-12-07 02:56:32 -0500

    (master) format

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,10 +1,14 @@
-use pino_wgpu_shape::{ShapeRenderer, Instance};
-use wgpu::SurfaceError;
-use winit::{event_loop::{EventLoop, ControlFlow}, window::WindowBuilder, event::{Event, WindowEvent}, dpi::LogicalSize};
 use cgmath::Vector3;
+use pino_wgpu_shape::{Instance, ShapeRenderer};
+use wgpu::SurfaceError;
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 fn main() {
-
     // bunch of setup boilerplate
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
@@ -17,49 +21,47 @@ fn main() {
     let surface = unsafe { instance.create_surface(&window) };
 
     let (device, queue) = futures::executor::block_on(async {
-	
-	let adapter = instance
-	    .request_adapter(&wgpu::RequestAdapterOptions {
-		power_preference: wgpu::PowerPreference::default(),
-		compatible_surface: Some(&surface),
-		force_fallback_adapter: false,
-	    })
-	    .await
-	    .unwrap();
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .unwrap();
 
-	adapter
-	    .request_device(
-		&wgpu::DeviceDescriptor {
-		    features: wgpu::Features::empty(),
-		    limits: if cfg!(target_arch = "wasm32") {
-			wgpu::Limits::downlevel_webgl2_defaults()
-		    } else {
-			wgpu::Limits::default()
-		    },
-		    label: None,
-		},
-		None,
-	    )
-	    .await
-	    .unwrap()
+        adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    limits: if cfg!(target_arch = "wasm32") {
+                        wgpu::Limits::downlevel_webgl2_defaults()
+                    } else {
+                        wgpu::Limits::default()
+                    },
+                    label: None,
+                },
+                None,
+            )
+            .await
+            .unwrap()
     });
-
 
     let render_format = wgpu::TextureFormat::Bgra8UnormSrgb;
     let mut size = window.inner_size();
     let mut config = wgpu::SurfaceConfiguration {
-	usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-	format: render_format,
-	width: size.width,
-	height: size.height,
-	present_mode: wgpu::PresentMode::Fifo,
-	alpha_mode: wgpu::CompositeAlphaMode::Auto,
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format: render_format,
+        width: size.width,
+        height: size.height,
+        present_mode: wgpu::PresentMode::Fifo,
+        alpha_mode: wgpu::CompositeAlphaMode::Auto,
     };
 
     surface.configure(&device, &config);
 
     let mut shape_renderer = ShapeRenderer::new(&device, render_format);
-    let mut staging_belt =  wgpu::util::StagingBelt::new(1024);
+    let mut staging_belt = wgpu::util::StagingBelt::new(1024);
 
     // run event loop
     event_loop.run(move |event, _, control_flow| match event {
@@ -71,65 +73,69 @@ fn main() {
                 match event {
                     WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
                     WindowEvent::Resized(physical_size) => {
-			let new_size = *physical_size;
-			if new_size.width > 0 && new_size.height > 0 {
-			    config.width = new_size.width;
-			    config.height = new_size.height;
-			    surface.configure(&device, &config);
-			}
-                    }
-                    _ => {}
+                        let new_size = *physical_size;
+                        if new_size.width > 0 && new_size.height > 0 {
+                            config.width = new_size.width;
+                            config.height = new_size.height;
+                            surface.configure(&device, &config);
+                        }
+                    },
+                    _ => {},
                 }
             }
         },
         Event::RedrawRequested(window_id) => {
+            let output = surface
+                .get_current_texture()
+                .expect("could not get texture");
 
-	    let output = surface.get_current_texture().expect("could not get texture");
+            let mut view = output
+                .texture
+                .create_view(&wgpu::TextureViewDescriptor::default());
+            let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
 
-	    let mut view = output
-		.texture
-		.create_view(&wgpu::TextureViewDescriptor::default());
-	    let mut encoder = device
-		.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-		    label: Some("Render Encoder"),
-	    });
+            let render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.0,
+                            g: 0.0,
+                            b: 0.0,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+            drop(render_pass);
 
-	    let render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-		label: Some("Render Pass"),
-		color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-		    view: &view,
-		    resolve_target: None,
-		    ops: wgpu::Operations {
-			load: wgpu::LoadOp::Clear(wgpu::Color {
-			    r: 0.0,
-			    g: 0.0,
-			    b: 0.0,
-			    a: 1.0,
-			}),
-			store: true,
-		    },
-		})],
-		depth_stencil_attachment: None,
-	    });
-	    drop(render_pass);
+            let instances = vec![
+                Instance {
+                    position: Vector3::new(0., 0., 0.),
+                    scale: Vector3::new(1., 1., 1.),
+                },
+                Instance {
+                    position: Vector3::new(1., 1., 1.),
+                    scale: Vector3::new(1., 1., 1.),
+                },
+            ];
+            for instance in instances {
+                shape_renderer.queue(instance);
+            }
+            shape_renderer.draw(&device, &mut encoder, &mut view, &mut staging_belt);
 
-	    let instances = vec![
-		Instance {position: Vector3::new(0., 0., 0.), scale: Vector3::new(1., 1., 1.)},
-		Instance {position: Vector3::new(1., 1., 1.), scale: Vector3::new(1., 1., 1.)}
-	    ];
-	    for instance in instances {
-		shape_renderer.queue(instance);
-	    }
-	    shape_renderer.draw(&device, &mut encoder, &mut view, &mut staging_belt);
+            staging_belt.finish();
+            queue.submit(std::iter::once(encoder.finish()));
+            output.present();
+            staging_belt.recall();
+        },
 
-	    staging_belt.finish();
-	    queue.submit(std::iter::once(encoder.finish()));
-	    output.present();
-	    staging_belt.recall();
-
-        }
-
-	_ => {}
+        _ => {},
     });
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,15 @@
 use std::{mem, num::NonZeroU64};
 
 use bytemuck::bytes_of;
+use cgmath::{num_traits::Pow, prelude, Vector3};
 use wgpu::{include_wgsl, util::DeviceExt};
-use cgmath::{prelude, num_traits::Pow, Vector3};
 
 const VERTEX_ENTRY_POINT: &'static str = "vs_main";
 const FRAGMENT_ENTRY_POINT: &'static str = "fs_main";
 
 pub struct Point2D {
     pub x: f32,
-    pub y: f32
+    pub y: f32,
 }
 
 pub enum Shape {
@@ -38,8 +38,8 @@ struct Vertex {
 }
 
 impl Vertex {
-
-    const ATTRIBS: [wgpu::VertexAttribute; 2] = wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3];
+    const ATTRIBS: [wgpu::VertexAttribute; 2] =
+        wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3];
 
     fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
         wgpu::VertexBufferLayout {
@@ -57,21 +57,23 @@ pub struct Instance {
 
 impl Instance {
     fn to_raw(&self) -> InstanceRaw {
-	InstanceRaw {
-	    model: (cgmath::Matrix4::from_translation(self.position) * cgmath::Matrix4::from_nonuniform_scale(self.scale.x, self.scale.y, self.scale.z)).into()
-	}
+        InstanceRaw {
+            model: (cgmath::Matrix4::from_translation(self.position)
+                * cgmath::Matrix4::from_nonuniform_scale(self.scale.x, self.scale.y, self.scale.z))
+            .into(),
+        }
     }
 }
 
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct InstanceRaw {
-    model: [[f32; 4]; 4]
+    model: [[f32; 4]; 4],
 }
 
 impl InstanceRaw {
-
-    const ATTRIBS: [wgpu::VertexAttribute; 4] = wgpu::vertex_attr_array![5 => Float32x4, 6 => Float32x4, 7 => Float32x4, 8 => Float32x4];
+    const ATTRIBS: [wgpu::VertexAttribute; 4] =
+        wgpu::vertex_attr_array![5 => Float32x4, 6 => Float32x4, 7 => Float32x4, 8 => Float32x4];
 
     fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
         wgpu::VertexBufferLayout {
@@ -84,13 +86,30 @@ impl InstanceRaw {
 
 // this is a square
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [-0.5, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
-    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
-    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
-
-    Vertex { position: [-0.5, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
-    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
-    Vertex { position: [0.5, 0.5, 0.0], color: [0.0, 1.0, 0.0] },
+    Vertex {
+        position: [-0.5, 0.5, 0.0],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        position: [-0.5, -0.5, 0.0],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [0.5, -0.5, 0.0],
+        color: [0.0, 0.0, 1.0],
+    },
+    Vertex {
+        position: [-0.5, 0.5, 0.0],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        position: [0.5, -0.5, 0.0],
+        color: [0.0, 0.0, 1.0],
+    },
+    Vertex {
+        position: [0.5, 0.5, 0.0],
+        color: [0.0, 1.0, 0.0],
+    },
 ];
 const INSTANCE_BUFFER_INIT_SIZE: usize = 64;
 
@@ -102,119 +121,121 @@ pub struct ShapeRenderer {
 }
 
 impl ShapeRenderer {
-
     pub fn new(device: &wgpu::Device, render_format: wgpu::TextureFormat) -> Self {
+        let shader = device.create_shader_module(include_wgsl!("shaders/shader.wgsl"));
 
-	let shader = device.create_shader_module(include_wgsl!("shaders/shader.wgsl"));
+        // create vertex buffer
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::cast_slice(VERTICES),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
 
-	// create vertex buffer
-	let vertex_buffer = device.create_buffer_init(
-	    &wgpu::util::BufferInitDescriptor {
-		label: Some("Vertex Buffer"),
-		contents: bytemuck::cast_slice(VERTICES),
-		usage: wgpu::BufferUsages::VERTEX,
-	    }
-	);
+        // create instance buffer
+        let instance_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Instance Buffer"),
+            size: mem::size_of::<Instance>() as u64 * INSTANCE_BUFFER_INIT_SIZE as u64,
+            usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
 
-	// create instance buffer
-	let instance_buffer = device.create_buffer(
-	    &wgpu::BufferDescriptor {
-		label: Some("Instance Buffer"),
-		size: mem::size_of::<Instance>() as u64 * INSTANCE_BUFFER_INIT_SIZE as u64,
-		usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
-		mapped_at_creation: false
-	    }
-	);
+        // create render pipeline
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
 
-	// create render pipeline
-	let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-	    label: Some("Render Pipeline Layout"),
-	    bind_group_layouts: &[],
-	    push_constant_ranges: &[],
-	});
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: VERTEX_ENTRY_POINT,
+                buffers: &[Vertex::desc(), InstanceRaw::desc()],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: FRAGMENT_ENTRY_POINT,
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: render_format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+        });
 
-	let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-	    label: Some("Render Pipeline"),
-	    layout: Some(&render_pipeline_layout),
-	    vertex: wgpu::VertexState {
-		module: &shader,
-		entry_point: VERTEX_ENTRY_POINT,
-		buffers: &[
-		    Vertex::desc(),
-		    InstanceRaw::desc(),
-		]
-	    },
-	    fragment: Some(wgpu::FragmentState {
-		module: &shader,
-		entry_point: FRAGMENT_ENTRY_POINT,
-		targets: &[Some(wgpu::ColorTargetState {
-		    format: render_format,
-		    blend: Some(wgpu::BlendState::REPLACE),
-		    write_mask: wgpu::ColorWrites::ALL,
-		})],
-	    }),
-	    primitive: wgpu::PrimitiveState {
-		topology: wgpu::PrimitiveTopology::TriangleList,
-		strip_index_format: None,
-		front_face: wgpu::FrontFace::Ccw,
-		cull_mode: Some(wgpu::Face::Back),
-		polygon_mode: wgpu::PolygonMode::Fill,
-		unclipped_depth: false,
-		conservative: false,
-	    },
-	    depth_stencil: None,
-	    multisample: wgpu::MultisampleState {
-		count: 1,
-		mask: !0,
-		alpha_to_coverage_enabled: false
-	    },
-	    multiview: None
-	});
-
-	ShapeRenderer {
-	    pipeline: render_pipeline,
-	    vertex_buffer,
-	    instances: vec![],
-	    instance_buffer,
-	}
+        ShapeRenderer {
+            pipeline: render_pipeline,
+            vertex_buffer,
+            instances: vec![],
+            instance_buffer,
+        }
     }
 
-    pub fn draw(&mut self, device: &wgpu::Device, encoder: &mut wgpu::CommandEncoder, view: &wgpu::TextureView, staging_belt: &mut wgpu::util::StagingBelt) {
+    pub fn draw(
+        &mut self,
+        device: &wgpu::Device,
+        encoder: &mut wgpu::CommandEncoder,
+        view: &wgpu::TextureView,
+        staging_belt: &mut wgpu::util::StagingBelt,
+    ) {
+        // copy the new instance data
+        let instance_data = self
+            .instances
+            .iter()
+            .map(Instance::to_raw)
+            .collect::<Vec<_>>();
+        let instances_bytes: &[u8] = bytemuck::cast_slice(&instance_data);
+        if let Some(size) = NonZeroU64::new(instances_bytes.len() as u64) {
+            let mut instances_view =
+                staging_belt.write_buffer(encoder, &self.instance_buffer, 0, size, device);
+            instances_view.copy_from_slice(instances_bytes);
+        }
 
-	// copy the new instance data
-	let instance_data = self.instances.iter().map(Instance::to_raw).collect::<Vec<_>>();
-	let instances_bytes: &[u8] = bytemuck::cast_slice(&instance_data);
-	if let Some(size) = NonZeroU64::new(instances_bytes.len() as u64) {
-	    let mut instances_view = staging_belt.write_buffer(encoder, &self.instance_buffer, 0, size, device);
-	    instances_view.copy_from_slice(instances_bytes);
-	}
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color {
+                        r: 1.0,
+                        g: 1.0,
+                        b: 0.3,
+                        a: 1.0,
+                    }),
+                    store: true,
+                },
+            })],
+            depth_stencil_attachment: None,
+        });
+        render_pass.set_pipeline(&self.pipeline);
+        render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+        render_pass.set_vertex_buffer(1, self.instance_buffer.slice(..));
+        render_pass.draw(0..(VERTICES.len() as u32), 0..self.instances.len() as _);
 
-	let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-	    label: Some("Render Pass"),
-	    color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-		view: &view,
-		resolve_target: None,
-		ops: wgpu::Operations {
-		    load: wgpu::LoadOp::Clear(wgpu::Color {
-			r: 1.0,
-			g: 1.0,
-			b: 0.3,
-			a: 1.0,
-		    }),
-		    store: true,
-		},
-	    })],
-	    depth_stencil_attachment: None,
-	});
-	render_pass.set_pipeline(&self.pipeline);
-	render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-	render_pass.set_vertex_buffer(1, self.instance_buffer.slice(..));
-	render_pass.draw(0..(VERTICES.len() as u32), 0..self.instances.len() as _);
-
-	self.instances.clear();
+        self.instances.clear();
     }
 
     pub fn queue(&mut self, instance: Instance) {
-	self.instances.push(instance);
+        self.instances.push(instance);
     }
 }
